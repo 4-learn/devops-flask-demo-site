16053e39c1382ead7bdfb555196ff147604604f1
yillkid <yillkid@gmail.com>
2024-03-16 14:44:50 +0000

    workshop: CD

--- server.py
+++ server.py
@@ -2,10 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["GET"])
+@app.route('/')
 def home():
-    return "OK"
+    return 'Hello, World!'
 
-if __name__ == "__main__":
-    app.debug = False
-    app.run(host = "0.0.0.0", port = 5001)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
